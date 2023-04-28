536365d3347effc45cd2fcae15b7b9353600d618
solofervqz <solofervazquez@gmail.com>
2023-04-28 12:58:43 -0600

    ADD: if-else-statement

---
+++ if-else-statement.py
@@ -0,0 +1,10 @@
+print("Hola, este programa usa if ... else")
+
+numero = int(input("Ingrese un número entero: "))
+
+if numero <= 15:
+    print("El numero que ingresaste es menor que 15")
+elif numero == 15:
+    print("El numero que ingresaste es 15")
+else:
+    print("El numero que ingresaste es mayor que 15")
